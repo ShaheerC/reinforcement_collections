4ef0b8cee0ea1512f1eaa1708aa83900de822b6c
Shaheer Choudhury <shaheer.choudhury@gmail.com>
2019-07-23 19:53:18 -0400

    finished reinforcement

--- reinforcement.py
+++ reinforcement.py
@@ -1,5 +1,9 @@
-hotel = { 'data': { 'rooms': 
-    [ { 'id': 1, 'room_number': "201", 'capacity': 50}, { 'id': 2, 'room_number': "301", 'capacity': 200 }, { 'id': 3, 'room_number': "1B", 'capacity': 100}
+hotel = { 
+  'data': { 
+    'rooms': [ 
+      { 'id': 1, 'room_number': "201", 'capacity': 50}, 
+      { 'id': 2, 'room_number': "301", 'capacity': 200 }, 
+      { 'id': 3, 'room_number': "1B", 'capacity': 100}
     ],
     'events': [
       { 'id': 1, 'room_id': 2, 'start_time': 18, 'end_time': 20, 'attendees': 75 },
@@ -13,12 +17,11 @@ hotel = { 'data': { 'rooms':
 capacity_201 = hotel['data']['rooms'][0]['capacity']
 print(capacity_201)
 
-
-events_201 = []
 for event in hotel['data']['events']:
-    if event['room_id'] == 1:
-        events_201.append(event)
-    if event['attendees'] <= hotel['data']['rooms'][(event['room_id']-1)]['capacity']:
-        print('ok') 
+  if event['room_id'] == 1:
+      if event['attendees'] > capacity_201:
+          print('Too many people')
+      else:
+          print('Ok')
 
-print(events_201)
+# event['id']
